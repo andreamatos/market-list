db89118dc33b3a1b50d56a4b73a7783587b3a672
André Matos <S671728@alwaysko.com>
2024-02-05 09:03:13 -0300

    incluindo mundanças

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,18 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent implements OnInit{
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
     recipes: Recipe[] = [
       new Recipe('A test Recipe', 'This is a simple test', 'https://cdn.pixabay.com/photo/2024/01/19/18/00/hydrangeas-8519528_1280.jpg'),
-      new Recipe('A test Recipe', 'This is a simple test', 'https://cdn.pixabay.com/photo/2024/01/19/18/00/hydrangeas-8519528_1280.jpg'),
+      new Recipe('Another Test Recipe', 'This is a simple test', 'https://cdn.pixabay.com/photo/2024/01/19/18/00/hydrangeas-8519528_1280.jpg'),
     ];
   
     constructor() { }
   
     ngOnInit(): void {
+    }
+
+    onRecipeSelect(recipe: Recipe){
+      this.recipeWasSelected.emit(recipe);
     }
 }
